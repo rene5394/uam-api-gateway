6f61e1110b7ee0193710421fa375b00b55a5ef4c
reneuam <renet@uassistme.com>
2022-08-11 10:45:02 -0600

    Add global interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalInterceptors(new TimeOutInterceptor);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
