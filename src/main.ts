ed2d80b30b0b4fe47d3dd6c273f998a3d9da4f07
reneuam <renet@uassistme.com>
2022-08-12 00:01:12 -0600

    Add ValidationPipe for class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true
+  }));
   app.useGlobalInterceptors(new TimeOutInterceptor);
   await app.listen(process.env.PORT || 8080);
 }
