402e1cb6d97781d3693486684aa2ae971de15792
reneuam <renet@uassistme.com>
2022-08-20 21:15:44 -0600

    Add entry module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BalanceModule } from './api/timeoff/balance/balance.module';
 import { ConfigModule } from '@nestjs/config';
+import { EntrieModule } from './api/attendance/entry/entry.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: ['.env'],
     isGlobal: true
   }),
-  BalanceModule],
+  BalanceModule,
+  EntrieModule],
   controllers: [AppController],
   providers: [AppService],
 })
