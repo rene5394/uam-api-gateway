920a6623bba6a2b480501feda3f11734b71ba9f4
reneuam <renet@uassistme.com>
2022-08-20 19:37:27 -0600

    Add entry proxy

---
+++ src/common/proxy/client-proxy-attendance.ts
@@ -0,0 +1,19 @@
+import { Injectable} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { RabbitMQ } from '../constants/rabbitmq';
+
+@Injectable()
+export class ClientProxyProxyAttendance  {
+  constructor(private readonly configService: ConfigService) {}
+
+  clientProxyEntrie(): ClientProxy {
+    return ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+          urls: this.configService.get('AMQP_URL'),
+          queue: RabbitMQ.AttendanceEntrieQueue
+      }
+    })
+  }
+}
