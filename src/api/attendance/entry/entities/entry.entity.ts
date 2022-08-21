f960f110e1c3d797dc21d7d9e0cb1d9e168851a2
reneuam <renet@uassistme.com>
2022-08-21 15:04:43 -0600

    Change field data types

--- src/api/attendance/entry/entities/entry.entity.ts
+++ src/api/attendance/entry/entities/entry.entity.ts
@@ -2,38 +2,38 @@ import {  Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity('attendance')
 export class Entry {
-    @PrimaryColumn({ type: 'smallint', nullable: false })
+    @PrimaryColumn({ type: 'int', nullable: false })
     id: number;
 
-    @Column({ type: 'smallint', nullable: false })
+    @Column({ type: 'int', nullable: false })
     employee_id: number;
 
-    @Column({ type: 'smallint', nullable: false })
+    @Column({ type: 'int', nullable: false })
     team_id: number;
 
-    @Column({ type: 'smallint', nullable: false })
-    date: number;
+    @Column({ type: 'datetime', nullable: false })
+    date: Date;
 
-    @Column({ type: 'smallint', nullable: false })
-    comment: number;
+    @Column({ type: 'varchar', nullable: false })
+    comment: string;
 
-    @Column({ type: 'smallint', nullable: false })
+    @Column({ type: 'int', nullable: false })
     user_id: number;
 
-    @Column({ type: 'smallint', nullable: false })
+    @Column({ type: 'int', nullable: false })
     attendance_status: number;
 
-    @Column({ type: 'tinyint', nullable: false })
+    @Column({ type: 'int', nullable: false })
     days: number;
 
-    @Column({ type: 'tinyint', nullable: false })
+    @Column({ type: 'int', nullable: false })
     update_by: number;
 
-    @Column({ type: 'tinyint', nullable: false })
-    created_at: number;
+    @Column({ type: 'datetime', nullable: false })
+    created_at: Date;
 
-    @Column({ type: 'tinyint', nullable: false })
-    updated_at: number;
+    @Column({ type: 'datetime', nullable: false })
+    updated_at: Date;
 
     @Column({ type: 'tinyint', nullable: false })
     paid: number;
