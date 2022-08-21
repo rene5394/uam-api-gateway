89562581151e1351c2e7351084e4dac401ab2025
reneuam <renet@uassistme.com>
2022-08-21 15:14:33 -0600

    Add update attendance entry validation

--- src/api/attendance/entry/dto/update-entry.dto.ts
+++ src/api/attendance/entry/dto/update-entry.dto.ts
@@ -1,4 +1,26 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateEntryDto } from './create-entry.dto';
+import { IsDateString, IsInt, IsOptional, Length, Max, Min } from "@nestjs/class-validator";
+export class UpdateEntryDto {
+  @IsOptional()
+  @Length(100)
+  comment: string;
 
-export class UpdateEntryDto extends PartialType(CreateEntryDto) {}
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  attendance_status: number;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  days: number;
+
+  @IsInt()
+  @Min(1)
+  update_by: number;
+
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  @Max(1)
+  paid: number;
+}
