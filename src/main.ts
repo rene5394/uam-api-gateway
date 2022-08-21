20ab3b2049a2cfa2f70bdd7295c2d2e404c26e52
reneuam <renet@uassistme.com>
2022-08-20 21:12:12 -0600

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));
