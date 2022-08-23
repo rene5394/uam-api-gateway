ca72030a14243fb89464615bd85eab4d4be6c1ec
reneuam <renet@uassistme.com>
2022-08-23 00:03:36 -0600

    Use authRepository instead userService, validate password and generate JWT with custom fields

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,25 +1,43 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from './users/users.service';
+import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
+import { Repository } from 'typeorm';
+import { Auth } from './auth.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private usersService: UsersService,
+    @InjectRepository(Auth)
+    private readonly authRepository: Repository<Auth>,
     private jwtService: JwtService
   ) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
-    if (user && user.password === pass) {
-      const { password, ...result } = user;
-      return result;
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.authRepository.findOne({ where : {email: email} });
+
+    if (user) {
+      let hash = user.password;
+      hash = hash.replace(/^\$2y(.+)$/i, '$2a$1');
+      const match = await bcrypt.compare(pass, hash);
+
+      if (match) {
+        const { password, ...result } = user;
+        return result;
+      }
     }
+
     return null;
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+    const payload = {
+      id: user.id,
+      email: user.email,
+      roleId: user.role_id,
+      hr: user.hr
+    };
+
     return {
       access_token: this.jwtService.sign(payload),
     };
