500c6d7ac1ec3aabb490b9d44d24444f452903ae
reneuam <renet@uassistme.com>
2022-08-23 00:17:12 -0600

    Folder for auth entity

---
+++ src/auth/entities/auth.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('users')
+export class Auth {
+    @PrimaryColumn({ type: 'int', nullable: false })
+    id: number;
+
+    @Column({ type: 'varchar', nullable: false })
+    email: string;
+
+    @Column({ type: 'varchar', nullable: false })
+    password: string;
+
+    @Column({ type: 'int', nullable: false })
+    role_id: number;
+
+    @Column({ type: 'tinyint', nullable: false })
+    hr: number;
+}
