832021ed964cd239c8cc733aed2e82c337d34f91
reneuam <renet@uassistme.com>
2022-08-23 00:18:59 -0600

    Change directory path of auth entity

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { Repository } from 'typeorm';
-import { Auth } from './auth.entity';
+import { Auth } from './entities/auth.entity';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
