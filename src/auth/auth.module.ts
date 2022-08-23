ed2ce3da58286d17c140d12a7c69e75ee7518df2
reneuam <renet@uassistme.com>
2022-08-23 00:15:21 -0600

    Delete call to unexisting user auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,17 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 import { JwtStrategy } from './jwt.strategy';
-import { UsersModule } from './users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
-import { Auth } from './auth.entity';
+import { Auth } from './entities/auth.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Auth]),
-    UsersModule,
     PassportModule,
     JwtModule.register({
       secret: String(process.env.JWT_SECRET),
