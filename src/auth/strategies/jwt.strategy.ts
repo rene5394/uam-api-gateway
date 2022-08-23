36be597f16375af501747c59800e07cef34aac69
reneuam <renet@uassistme.com>
2022-08-23 09:59:20 -0600

    Change strategies files location

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -13,8 +13,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { 
-      userId: payload.id, 
+    return {
+      userId: payload.id,
       email: payload.email,
       roleId: payload.roleId,
       hr: payload.hr
