86ad342fe466cdd418b6f1ff5e9bb9b94a5ebe6e
reneuam <renet@uassistme.com>
2022-08-23 00:00:01 -0600

    Add auth controller and auth repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { BalanceModule } from './api/timeoff/balance/balance.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EntrieModule } from './api/attendance/entry/entry.module';
 import { StatusModule } from './api/attendance/status/status.module';
 import { AuthModule } from './auth/auth.module';
@@ -12,11 +12,21 @@ import { UsersModule } from './auth/users/users.module';
     envFilePath: ['.env'],
     isGlobal: true
   }),
+  TypeOrmModule.forRoot({
+    type: process.env.DATABASE_TYPE as any || 'mysql',
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT) || 3306,
+    username  : process.env.DATABASE_USERNAME || 'root',
+    password  : process.env.DATABASE_PASSWORD || 'password',
+    database: process.env.DATABASE || 'uamapp',
+    entities: [],
+    autoLoadEntities: true,
+    synchronize: false
+  }),
   BalanceModule,
   EntrieModule,
   StatusModule,
   AuthModule,
-  UsersModule],
-  controllers: [AppController]
+  UsersModule]
 })
 export class AppModule {}
