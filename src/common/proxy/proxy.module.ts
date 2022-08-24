818c5618223479b12c7b3b2f6fefcf2885aa3b0d
reneuam <renet@uassistme.com>
2022-08-24 16:07:03 -0600

    Add client proxy team

--- src/common/proxy/proxy.module.ts
+++ src/common/proxy/proxy.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ClientProxyAttendance } from './client-proxy-attendance';
+import { ClientProxyTeam } from './client-proxy-team';
 import { ClientProxyTimeOff } from './client-proxy-timeoff';
 
 @Module({
-  providers: [ClientProxyTimeOff, ClientProxyAttendance],
-  exports: [ClientProxyTimeOff, ClientProxyAttendance]
+  providers: [ClientProxyTimeOff, ClientProxyTeam, ClientProxyAttendance],
+  exports: [ClientProxyTimeOff, ClientProxyTeam, ClientProxyAttendance]
 })
 export class ProxyModule {}
