ecc7a30e56c4ef3a2599df55e73e740e2ceb3b50
reneuam <renet@uassistme.com>
2022-08-29 21:50:56 -0600

    Add proxy module to balanceTransaction module

--- src/api/timeoff/balance-transaction/balance-transaction.module.ts
+++ src/api/timeoff/balance-transaction/balance-transaction.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ProxyModule } from 'src/common/proxy/proxy.module';
 import { BalanceTransactionController } from './balance-transaction.controller';
 
 @Module({
+  imports: [ProxyModule],
   controllers: [BalanceTransactionController]
 })
 export class BalanceTransactionModule {}
