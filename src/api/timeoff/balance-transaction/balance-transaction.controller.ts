2b3763b336914c49cab3a4b427ed9640d17a0d3e
reneuam <renet@uassistme.com>
2022-08-29 22:02:55 -0600

    Add guards for balanceTransaction controller

--- src/api/timeoff/balance-transaction/balance-transaction.controller.ts
+++ src/api/timeoff/balance-transaction/balance-transaction.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Auth } from 'src/common/decorators/auth.decorator';
+import { JwtAuthGuard } from '../../../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../../../auth/guards/roles.guard';
+import { Roles } from '../../../common/decorators/role.decorator';
+import { Role } from '../../../common/enums/role.enum';
 import { CreateBalanceTransactionDto } from './dto/create-balance-transaction.dto';
 import { UpdateBalanceTransactionDto } from './dto/update-balance-transaction.dto';
 
 @ApiTags('Balance Transactions')
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('balance-transaction')
 export class BalanceTransactionController {
   constructor(private readonly balanceTransactionService: BalanceTransactionService) {}
