39881851aa4965dd4bac81c96e6181abced3caee
reneuam <renet@uassistme.com>
2022-08-31 00:09:53 -0600

    Set findAll and finOne functions

--- src/api/timeoff/status/status.controller.ts
+++ src/api/timeoff/status/status.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, HttpException, HttpStatus } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Auth } from 'src/common/decorators/auth.decorator';
 import { JwtAuthGuard } from '../../../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../../../auth/guards/roles.guard';
 import { Roles } from '../../../common/decorators/role.decorator';
 import { Role } from '../../../common/enums/role.enum';
+import { Observable } from 'rxjs';
+import { StatusMSG } from 'src/common/constants/time-off-messages';
 import { ClientProxyTimeOff } from 'src/common/proxy/client-proxy-timeoff';
+import { Status } from './entities/status.entity';
 
 @ApiTags('Request Statuses')
 @UseGuards(JwtAuthGuard, RolesGuard)
@@ -17,13 +20,29 @@ export class StatusController {
 
   @Roles(Role.admin, Role.coach, Role.jrCoach, Role.va)
   @Get()
-  findAll() {
-    return this.statusService.findAll();
+  findAll(): Observable<Status[]> {
+    return this.clientProxyType.send(StatusMSG.FIND_ALL, '');
   }
 
   @Roles(Role.admin, Role.coach, Role.jrCoach, Role.va)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.statusService.findOne(+id);
+  async findOne(@Param('id') id: number): Promise<Observable<Status>> {
+    const status = this.clientProxyType.send(StatusMSG.FIND_ONE, id);
+
+    const statusFound = await new Promise<boolean>(resolve =>
+      status.subscribe(result => {
+        if (!result) {
+         resolve(false);
+        }
+       
+        resolve(true);
+      })
+    );
+
+    if (!statusFound) {
+      throw new HttpException('NOT FOUND', HttpStatus.NOT_FOUND);
+    }
+
+    return status;
   }
 }
