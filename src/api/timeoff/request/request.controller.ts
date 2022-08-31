b1003ead2564ee3565fb59075bf5eacf3c6fc257
reneuam <renet@uassistme.com>
2022-08-31 07:17:52 -0600

    Add proxy

--- src/api/timeoff/request/request.controller.ts
+++ src/api/timeoff/request/request.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { ClientProxyTimeOff } from 'src/common/proxy/client-proxy-timeoff';
 import { CreateRequestDto } from './dto/create-request.dto';
 import { UpdateRequestDto } from './dto/update-request.dto';
 
 @ApiTags('Timeoff Requests')
 @Controller('request')
 export class RequestController {
-  constructor(private readonly requestService: RequestService) {}
+  constructor(private readonly clientProxy: ClientProxyTimeOff) {}
+
+  private clientProxyRequest = this.clientProxy.clientProxyBalance();
 
   @Post()
   create(@Body() createRequestDto: CreateRequestDto) {
