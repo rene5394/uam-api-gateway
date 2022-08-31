39760f3d3fd0017d2be054cbb2990949a804890b
reneuam <renet@uassistme.com>
2022-08-30 23:35:48 -0600

    Add proxy module to type module

--- src/api/timeoff/type/type.module.ts
+++ src/api/timeoff/type/type.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ProxyModule } from 'src/common/proxy/proxy.module';
 import { TypeController } from './type.controller';
 
 @Module({
+  imports: [ProxyModule],
   controllers: [TypeController]
 })
 export class TypeModule {}
