35968747eb99d904bea37e6f10a8dc4918ddc002
reneuam <renet@uassistme.com>
2022-08-31 06:51:40 -0600

    Add request controller api tag

--- src/api/timeoff/request/request.controller.ts
+++ src/api/timeoff/request/request.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { RequestService } from './request.service';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateRequestDto } from './dto/create-request.dto';
 import { UpdateRequestDto } from './dto/update-request.dto';
 
+@ApiTags('Timeoff Requests')
 @Controller('request')
 export class RequestController {
   constructor(private readonly requestService: RequestService) {}
