bade7be160522e38a0efe9e683055ff73ad1b318
reneuam <renet@uassistme.com>
2022-08-30 23:37:03 -0600

    Add API tag

--- src/api/timeoff/type/type.controller.ts
+++ src/api/timeoff/type/type.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Request Types')
 @Controller('type')
 export class TypeController {
   constructor(private readonly typeService: TypeService) {}
