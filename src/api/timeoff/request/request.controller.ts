509fefcc26a4332c9a29f994c5931379ce8c5a59
reneuam <renet@uassistme.com>
2022-08-31 07:27:40 -0600

    Set guards

--- src/api/timeoff/request/request.controller.ts
+++ src/api/timeoff/request/request.controller.ts
@@ -1,38 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Auth } from 'src/common/decorators/auth.decorator';
+import { JwtAuthGuard } from '../../../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../../../auth/guards/roles.guard';
+import { Roles } from '../../../common/decorators/role.decorator';
+import { Role } from '../../../common/enums/role.enum';
 import { ClientProxyTimeOff } from 'src/common/proxy/client-proxy-timeoff';
 import { CreateRequestDto } from './dto/create-request.dto';
 import { UpdateRequestDto } from './dto/update-request.dto';
 
 @ApiTags('Timeoff Requests')
-@Controller('request')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Controller('v1/timeoff/requests')
 export class RequestController {
   constructor(private readonly clientProxy: ClientProxyTimeOff) {}
 
   private clientProxyRequest = this.clientProxy.clientProxyBalance();
 
+  @Roles(Role.admin)
   @Post()
   create(@Body() createRequestDto: CreateRequestDto) {
     return this.requestService.create(createRequestDto);
   }
 
+  @Roles(Role.admin, Role.coach, Role.jrCoach, Role.va)
+  @Post()
+  createByUserJWT(@Auth() auth, @Body() createRequestDto: CreateRequestDto) {
+    return this.requestService.create(createRequestDto);
+  }
+
+  @Roles(Role.admin)
   @Get()
   findAll() {
     return this.requestService.findAll();
   }
 
+  @Roles(Role.admin, Role.coach, Role.jrCoach, Role.va)
+  @Get()
+  findAllByUserJWT(@Auth() auth) {
+    return this.requestService.findAll();
+  }
+
+  @Roles(Role.admin)
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: number) {
     return this.requestService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRequestDto: UpdateRequestDto) {
-    return this.requestService.update(+id, updateRequestDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.requestService.remove(+id);
   }
 }
