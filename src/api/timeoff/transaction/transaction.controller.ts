46062ca0ddafdcf926624e97104409221be524b9
reneuam <renet@uassistme.com>
2022-09-01 14:05:22 -0600

    Add API tag, guards and funcitons findByUserId, findbyRequestId

--- src/api/timeoff/transaction/transaction.controller.ts
+++ src/api/timeoff/transaction/transaction.controller.ts
@@ -1,34 +1,56 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { TransactionService } from './transaction.service';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../../../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../../../auth/guards/roles.guard';
+import { Roles } from '../../../common/decorators/role.decorator';
+import { Role } from '../../../common/enums/role.enum';
+import { Observable } from 'rxjs';
+import { ClientProxyTimeOff } from 'src/common/proxy/client-proxy-timeoff';
+import { TransctionMSG } from '../../../common/constants/time-off-messages';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { Transaction } from './entities/transaction.entity';
 
-@Controller('transaction')
+@ApiTags('Timeoff Transactions')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Controller('v1/timeoff/transactions')
 export class TransactionController {
-  constructor(private readonly transactionService: TransactionService) {}
+  constructor(private readonly clientProxy: ClientProxyTimeOff) {}
 
+  private clientProxyTransaction = this.clientProxy.clientProxyTransaction();
+
+  @Roles(Role.admin)
   @Post()
-  create(@Body() createTransactionDto: CreateTransactionDto) {
-    return this.transactionService.create(createTransactionDto);
+  create(@Body() createTransactionDto: CreateTransactionDto): Observable<Transaction> {
+    return this.clientProxyTransaction.send(TransctionMSG.CREATE, createTransactionDto);
   }
 
+  @Roles(Role.admin)
   @Get()
-  findAll() {
-    return this.transactionService.findAll();
+  findAll(): Observable<Transaction[]> {
+    return this.clientProxyTransaction.send(TransctionMSG.FIND_ALL, '');
   }
 
+  @Roles(Role.admin)
+  @Get(':userId')
+  findOneByUserId(@Param('userId') userId: number): Observable<Transaction[]> {
+    return this.clientProxyTransaction.send(TransctionMSG.FIND_ALL_USER_ID, userId);
+  }
+
+  @Roles(Role.admin, Role.coach, Role.jrCoach, Role.va)
+  @Get(':userId')
+  findOneByUserJWT(@Param('userId') userId: number): Observable<Transaction[]> {
+    return this.clientProxyTransaction.send(TransctionMSG.FIND_ALL_USER_ID, userId);
+  }
+
+  @Roles(Role.admin)
+  @Get(':requestId')
+  findOneByRequestId(@Param('requestId') requestId: number): Observable<Transaction[]> {
+    return this.clientProxyTransaction.send(TransctionMSG.FIND_ALL_REQUEST_ID, requestId);
+  }
+
+  @Roles(Role.admin)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.transactionService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
-    return this.transactionService.update(+id, updateTransactionDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.transactionService.remove(+id);
+  findOne(@Param('id') id: number): Observable<Transaction> {
+    return this.clientProxyTransaction.send(TransctionMSG.FIND_ONE, id);
   }
 }
