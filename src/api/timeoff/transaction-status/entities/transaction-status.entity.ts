52b2d8b2ae23a0df23063dba3f2688ec8dda41bb
reneuam <renet@uassistme.com>
2022-09-01 23:29:54 -0600

    Set fields of transaction status entity

--- src/api/timeoff/transaction-status/entities/transaction-status.entity.ts
+++ src/api/timeoff/transaction-status/entities/transaction-status.entity.ts
@@ -1,1 +1,16 @@
-export class TransactionStatus {}
+import {  Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('transaction-statuses')
+export class TransactionStatus {
+    @PrimaryColumn({ type: 'tinyint', nullable: false })
+    id: number;
+
+    @Column({ type: 'varchar', nullable: false })
+    name: number;
+
+    @Column({ type: 'datetime' })
+    createdAt: Date;
+
+    @Column({ type: 'datetime' })
+    updatedAt: Date;
+}
