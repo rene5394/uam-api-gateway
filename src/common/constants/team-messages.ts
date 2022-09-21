fc574ace41711ac76ae54c404f8fcafeac6d7a38
reneuam <renet@uassistme.com>
2022-09-21 14:06:06 -0600

    Add employee and member rabbitmq messages

--- src/common/constants/team-messages.ts
+++ src/common/constants/team-messages.ts
@@ -1,11 +1,19 @@
-export enum UserMSG {
-  FIND_ALL =  'findAllUser',
-  FIND_ALL_EMPLOYEES = 'findAllUserEmployee',
-  FIND_ONE = 'findOneUser'
+export enum EmployeeMSG {
+  FIND_ALL =  'findAllEmployee'
+}
+
+export enum MemberMSG {
+  FIND_ALL =  'findAllMember'
 }
 
 export enum TeamMSG {
   FIND_ALL = 'findAllTeam',
   FIND_ONE = 'findOneTeam',
   FIND_ONE_USER_ID = 'findOneTeamUserid'
+}
+
+export enum UserMSG {
+  FIND_ALL =  'findAllUser',
+  FIND_ALL_EMPLOYEES = 'findAllUserEmployee',
+  FIND_ONE = 'findOneUser'
 }
