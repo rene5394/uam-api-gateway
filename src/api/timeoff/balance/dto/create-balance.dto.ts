d33cd4ec553198eff932fd2c0affae072f1b4d70
reneuam <renet@uassistme.com>
2022-09-29 09:01:57 -0600

    Set max compDays and vacationDays condition to 99

--- src/api/timeoff/balance/dto/create-balance.dto.ts
+++ src/api/timeoff/balance/dto/create-balance.dto.ts
@@ -1,4 +1,4 @@
-import { IsInt, IsOptional, Min } from "class-validator";
+import { IsInt, IsOptional, Max, Min } from "class-validator";
 
 export class CreateBalanceDto {
   @IsInt()
@@ -8,10 +8,12 @@ export class CreateBalanceDto {
   @IsOptional()
   @IsInt()
   @Min(0)
+  @Max(99)
   compDays: number;
 
   @IsOptional()
   @IsInt()
   @Min(0)
+  @Max(99)
   vacationDays: number;
 }
--- src/api/timeoff/balance/dto/update-balance.dto.ts
+++ src/api/timeoff/balance/dto/update-balance.dto.ts
@@ -1,13 +1,15 @@
-import { IsInt, IsOptional, Min } from 'class-validator';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
 
 export class UpdateBalanceDto {
   @IsOptional()
   @IsInt()
   @Min(0)
+  @Max(99)
   compDays: number;
 
   @IsOptional()
   @IsInt()
   @Min(0)
+  @Max(99)
   vacationDays: number;
 }
