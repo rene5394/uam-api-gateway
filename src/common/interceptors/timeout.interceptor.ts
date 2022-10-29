ccf7766d64ec27f2cb270205ffb9c4ff1227b0d4
reneuam <renet@uassistme.com>
2022-10-29 10:32:23 -0600

    Reduce time of timeout interceptor

--- src/common/interceptors/timeout.interceptor.ts
+++ src/common/interceptors/timeout.interceptor.ts
@@ -1,11 +1,21 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor} from '@nestjs/common'
-import { Observable } from 'rxjs';
-import { timeout } from 'rxjs/operators';
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor, RequestTimeoutException} from '@nestjs/common'
+import { Observable, throwError, TimeoutError } from 'rxjs';
+import { catchError, timeout } from 'rxjs/operators';
 
 @Injectable()
 export class TimeOutInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler<any>)
   : Observable<any> | Promise<Observable<any>> {
-    return next.handle().pipe(timeout(120000));
-  }
+    console.log('This is an interceptor if TIMEOUT SS');
+    
+    return next.handle().pipe(
+      timeout(30000),
+      catchError(err => {
+        if (err instanceof TimeoutError) {
+          return throwError(() => new RequestTimeoutException());
+        }
+        return throwError(() => err);
+      })
+    );
+  };
 }
