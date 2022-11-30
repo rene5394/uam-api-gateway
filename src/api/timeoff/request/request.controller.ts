fd2f4a91b22bd2c095305ac0509d491317dd7e12
reneuam <renet@uassistme.com>
2022-11-30 00:27:16 -0600

    Add email actions commented after create and createByUserJWT on request controller

--- src/api/timeoff/request/request.controller.ts
+++ src/api/timeoff/request/request.controller.ts
@@ -6,18 +6,22 @@ import { JwtAuthGuard } from '../../../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../../../auth/guards/roles.guard';
 import { Roles } from '../../../common/decorators/role.decorator';
 import { Role } from '../../../common/enums/role.enum';
-import { RequestMSG } from 'src/common/constants/time-off-messages';
+import { EmailService } from 'src/api/email/email.service';
 import { ClientProxies } from 'src/common/proxy/client-proxies';
 import { CreateRequestDto } from './dto/create-request.dto';
 import { CreateRequestMeDto } from './dto/create-request-me.dto';
 import { lastValueFrom, Observable } from 'rxjs';
+import { RequestMSG } from 'src/common/constants/time-off-messages';
 import { UserMSG } from 'src/common/constants/team-messages';
 
 @ApiTags('Timeoff Requests')
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('v1/timeoff/requests')
 export class RequestController {
-  constructor(private readonly clientProxy: ClientProxies) {}
+  constructor(
+    private readonly clientProxy: ClientProxies,
+    private readonly emailService: EmailService
+  ) {}
 
   private clientProxyTimeOff = this.clientProxy.clientProxyTimeOff();
   private clientProxyTeam = this.clientProxy.clientProxyTeam();
@@ -27,23 +31,43 @@ export class RequestController {
   async create(@Auth() auth, @Body() createRequestDto: CreateRequestDto) {
     createRequestDto.createdBy = auth.userId;
 
-    try {
-      const user = this.clientProxyTeam.send(UserMSG.FIND_ONE, createRequestDto.userId);
-      const userFound  = await lastValueFrom(user);      
+    const user = this.clientProxyTeam.send(UserMSG.FIND_ONE, createRequestDto.userId);
+    const userFound  = await lastValueFrom(user)
+      .then((value: any) => {
+        return value;
+      })
+      .catch((e) => {
+        throw new HttpException(e, HttpStatus.BAD_REQUEST);
+      });
 
-      if (!userFound) {
-        throw new HttpException('NOT_FOUND', HttpStatus.NOT_FOUND);
+    if (!userFound) {
+      throw new HttpException('NOT_FOUND', HttpStatus.NOT_FOUND);
+    }
+
+    createRequestDto.roleId = userFound.role_id;
+    createRequestDto.coachApproval = 1;
+
+    const request = this.clientProxyTimeOff.send(RequestMSG.CREATE, createRequestDto);
+    const requestCreated = await lastValueFrom(request)
+      .then((value: any) => {
+        return value;
+      })
+      .catch((e) => {
+        throw new HttpException(e, HttpStatus.BAD_REQUEST);
+      });
+
+      if (!requestCreated) {
+        throw new HttpException('CONFLICT', HttpStatus.CONFLICT);
       }
 
-      createRequestDto.roleId = userFound.role_id;
-      createRequestDto.coachApproval = 1;
-      const request = this.clientProxyTimeOff.send(RequestMSG.CREATE, createRequestDto);
-      const requestFound = await lastValueFrom(request);
-      
-      return requestFound;
-    } catch (err) {
-      throw new HttpException(err, HttpStatus.BAD_REQUEST);
-    }
+      const emailData = {
+        user: userFound,
+        request: requestCreated
+      }
+
+      //this.emailService.requestCreatedByHr(emailData);
+    
+    return requestCreated;
   }
 
   @Roles(Role.admin, Role.coach, Role.jrCoach, Role.va)
@@ -54,14 +78,27 @@ export class RequestController {
     createRequestMeDto.roleId = auth.roleId;
     createRequestMeDto.coachApproval = 0;
 
-    try {
-      const request = this.clientProxyTimeOff.send(RequestMSG.CREATE_USER_ID, createRequestMeDto);
-      const requestFound = await lastValueFrom(request);
-      
-      return requestFound;
-    } catch (err) {
-      throw new HttpException(err, HttpStatus.BAD_REQUEST);
+    const request = this.clientProxyTimeOff.send(RequestMSG.CREATE_USER_ID, createRequestMeDto);
+    const requestCreated = await lastValueFrom(request)
+      .then((value: any) => {
+        return value;
+      })
+      .catch((e) => {
+        throw new HttpException(e, HttpStatus.BAD_REQUEST);
+      });
+
+    if (!requestCreated) {
+      throw new HttpException('CONFLICT', HttpStatus.CONFLICT);
     }
+
+    const emailData = {
+      user: auth,
+      request: requestCreated
+    }
+
+    //this.emailService.requestCreatedByUser(emailData);
+    
+    return requestCreated;
   }
 
   @Roles(Role.admin, Role.coach, Role.jrCoach)
